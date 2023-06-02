2b7e3e2b35554af659664c7f02df4735b5f669ac
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-06-03 01:47:42 +0200

    feat: OrganizerManager.razor

---
+++ Server/Controllers/EventController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsManager.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EventController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EventController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }   
+}
