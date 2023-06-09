bb56a299bc709afc7536e00136b951eccd0c5f01
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-06-09 14:44:11 +0200

    feat: handle staf role when delete event

--- Server/Handlers/Commands/Events/Delete/DeleteEventCommandHandler.cs
+++ Server/Handlers/Commands/Events/Delete/DeleteEventCommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿using EventsManager.Server.Data;
+using EventsManager.Server.Models;
+using EventsManager.Shared.Enums;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventsManager.Server.Handlers.Commands.Events.Delete;
@@ -7,23 +10,43 @@ namespace EventsManager.Server.Handlers.Commands.Events.Delete;
 public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommandRequest>
 {
     private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public DeleteEventCommandHandler(ApplicationDbContext context)
+    public DeleteEventCommandHandler(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
     
     public async Task Handle(DeleteEventCommandRequest request, CancellationToken cancellationToken)
     {
         var eventToDelete = await _context.Events
             .Include(x => x.Owner)
+            .Include(x => x.Registrations.Where(r => r.State == RegistrationState.Accepted && r.Role == RegistrationRole.Staff))
+            .ThenInclude(x => x.User)
             .SingleAsync(e => e.Id == request.EventId, cancellationToken: cancellationToken);
 
         if (eventToDelete.Owner.Id != request.UserId)
         {
             throw new Exception("You are not the owner of this event");
         }
+
+        var acceptedStaffRegistrations = eventToDelete.Registrations;
         
+        foreach (var registration in acceptedStaffRegistrations)
+        {
+            var otherAcceptedStaffRegistrationCount = await _context.Registrations
+                .CountAsync(x =>
+                    x.User.Id == registration.UserId && x.Event.Id != eventToDelete.Id &&
+                    x.State == RegistrationState.Accepted && x.Role == RegistrationRole.Staff, cancellationToken: cancellationToken);
+
+            if (otherAcceptedStaffRegistrationCount == 0)
+            {
+                var user = await _userManager.FindByIdAsync(registration.UserId);
+                await _userManager.RemoveFromRoleAsync(user, "Staff");
+            }
+        }
+
         _context.Remove(eventToDelete);
         await _context.SaveChangesAsync(cancellationToken);
     }
