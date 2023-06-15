bc7d458d00279f8d80f49648e8ea0ae5fc85b784
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-06-15 22:55:08 +0200

    feat: edit prices

--- Server/Controllers/EventPriceController.cs
+++ Server/Controllers/EventPriceController.cs
@@ -39,9 +39,9 @@ public class EventPriceController : ControllerBase
             return Conflict("Event is free. You can't add a price");
         }
         
-        if (priceRequest.Price < 1)
+        if (priceRequest.Price < 0.5m)
         {
-            return Conflict("Price must be greater than 0");
+            return Conflict("Price must be greater than 0.5");
         }
 
         sportEvent.Prices.Add(new EventPrice(priceRequest.Price, priceRequest.StartDate.ToUniversalTime(), priceRequest.EndDate.ToUniversalTime()));
@@ -74,6 +74,38 @@ public class EventPriceController : ControllerBase
         }
         
         _context.EventPrices.Remove(price);
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return Ok();
+    }   
+    
+    [HttpPut("{priceId:guid}")]
+    [Authorize(Roles = "Organizer")]
+    public async Task<ActionResult> Update([FromRoute] Guid priceId,[FromBody] EventPriceDto priceRequest, CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+     
+        var price = await _context.EventPrices
+            .Include(x => x.Event)
+            .ThenInclude(x => x.Owners)
+            .Include(x => x.Event)
+            .ThenInclude(x => x.Creator)
+            .SingleAsync(x => x.Id == priceId, cancellationToken);
+
+        if(price.Event.Owners.All(o => o.Id != userId) && price.Event.CreatorId != userId)   
+        {
+            return Unauthorized();
+        }
+
+        if (priceRequest.Price < 0.5m)
+        {
+            return Conflict("Price must be greater than 0.5");
+        }
+        
+        price.Price = priceRequest.Price;
+        price.StartDate = priceRequest.StartDate.ToUniversalTime();
+        price.EndDate = priceRequest.EndDate.ToUniversalTime();
+        
         await _context.SaveChangesAsync(cancellationToken);
         
         return Ok();
