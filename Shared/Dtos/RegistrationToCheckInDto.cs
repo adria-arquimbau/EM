e881d174903f7cbff703119ae157b64466183a2c
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-06-18 20:07:56 +0200

    feat: created check in page

---
+++ Shared/Dtos/RegistrationToCheckInDto.cs
@@ -0,0 +1,15 @@
+﻿using EventsManager.Shared.Enums;
+
+namespace EventsManager.Shared.Dtos;
+
+public class RegistrationToCheckInDto
+{
+    public Guid Id { get; set; }
+    public DateTime CreationDate { get; set; }
+    public RegistrationRole Role { get; set; }
+    public RegistrationState State { get; set; }
+    public int? Bib { get; set; }
+    public bool CheckedIn { get;  set; }
+    public UserDto RegisteredUser { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+}
